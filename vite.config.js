065f225cdf6a31c4dae89fed5624c31ab813bba5
ApekshaPai18 <apekshapai18@gmail.com>
2025-07-16 20:39:48 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
+  base: '/career-path-recommender/',
   plugins: [
     react(),
     tailwindcss(),],
